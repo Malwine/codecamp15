6dd06dbcb6aa10dcbc6725b7b4c9603ed927b72f
Malwine <malwine@mailbox.org>
2015-07-25 10:40:53 +0200

    Add white canvas

--- Geometrie.java
+++ Geometrie.java
@@ -1,15 +1,30 @@
 package codeCampPZ1;
 
-import javax.swing.JFrame;
+import java.awt.*;
+import javax.swing.*;
 
 public class Geometrie extends JFrame {
 	
-	Geometrie() {
+	Canvas canvas;
+	
+	private Geometrie() {
 		super("Objekte zeichnen");
+		
+		canvas = new Canvas();
+		canvas.setBackground(Color.WHITE);
+		this.add(canvas);
 		
 		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		this.setSize(600, 400);
 		this.setVisible(true);
+	}
+	
+	class Canvas extends JPanel {
+		@Override
+		protected void paintComponent(Graphics g) {
+			super.paintComponent(g);
+			Graphics2D g2 = (Graphics2D) g;
+		}
 	}
 
 	public static void main(String[] args) {

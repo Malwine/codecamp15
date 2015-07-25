52df6e63452a3a8bc449af448c2607aa76a5f2f6
Malwine <malwine@mailbox.org>
2015-07-25 12:24:58 +0200

    Draw circles in the middle

--- Geometrie.java
+++ Geometrie.java
@@ -3,10 +3,12 @@ package codeCampPZ1;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
 
 import javax.swing.*;
 
-public class Geometrie extends JFrame implements ActionListener {
+public class Geometrie extends JFrame implements ActionListener, MouseListener {
 	
 	Canvas canvas;
 	JPanel southPanel;
@@ -14,11 +16,14 @@ public class Geometrie extends JFrame implements ActionListener {
 	JToggleButton quadrate;
 	JToggleButton linien;
 	ButtonGroup buttonGroup;
+	boolean clickedLeft;
+	boolean clickedRight;
 	
 	private Geometrie() {
 		super("Objekte zeichnen");
 		
 		canvas = new Canvas();
+		canvas.addMouseListener(this);
 		this.add(canvas);
 		
 		southPanel = initSouthPanel();
@@ -39,14 +44,25 @@ public class Geometrie extends JFrame implements ActionListener {
 			super.paintComponent(g);
 			Graphics2D g2 = (Graphics2D) g;
 			
+			g2.setColor(Color.RED);
+			
+			int width = this.getWidth();
+			int height = this.getHeight();
+			
 			if(quadrate.isSelected()){
-				int width = this.getWidth();
-				int height = this.getHeight();
-				
 				g2.drawLine(0, 0, width/2, height);
 				g2.drawLine(0, height, width/2, 0);
 				g2.drawLine(width/2, 0, width, height);
 				g2.drawLine(width/2, height, width, 0);
+			}
+			
+			if(clickedLeft) {
+				g2.drawRect(width/8, height/4, width/4, height/2);
+				g2.fillRect(width/8, height/4, width/4, height/2);
+			}
+			else if(clickedRight) {
+				g2.drawRect(width/8*5, height/4, width/4, height/2);
+				g2.fillRect(width/8*5, height/4, width/4, height/2);
 			}
 		}
 	}
@@ -96,5 +112,31 @@ public class Geometrie extends JFrame implements ActionListener {
 		}
 		
 	}
+
+	@Override
+	public void mouseClicked(MouseEvent e) {
+		if (e.getX() < canvas.getWidth()/2){
+			clickedLeft = true;
+			clickedRight = false;
+			canvas.repaint();
+		}
+		else {
+			clickedLeft = false;
+			clickedRight = true;
+			canvas.repaint();
+		}
+	}
+
+	@Override
+	public void mousePressed(MouseEvent e) {}
+
+	@Override
+	public void mouseReleased(MouseEvent e) {}
+
+	@Override
+	public void mouseEntered(MouseEvent e) {}
+
+	@Override
+	public void mouseExited(MouseEvent e) {}
 
 }

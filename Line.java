6d1ba0f3098d3706bbcb47781b568d9c3b87b70c
Malwine <malwine@mailbox.org>
2015-07-25 15:31:55 +0200

    Add lines class

---
+++ Line.java
@@ -0,0 +1,18 @@
+package codeCampPZ1;
+
+import java.awt.Point;
+
+public class Line {
+	
+	int x1, y1, x2, y2;
+	Point startPoint, endPoint;
+	
+	Line(Point sPoint, Point ePoint){
+		startPoint = sPoint;
+		endPoint = ePoint;
+		this.x1 = sPoint.x;
+		this.y1 = sPoint.y;
+		this.x2 = ePoint.x;
+		this.x2 = ePoint.y;
+	}
+}
